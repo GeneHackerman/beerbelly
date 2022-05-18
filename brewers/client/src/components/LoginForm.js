3d7bc85f22e628326e4b23644c53c631f61f85d9
Mariana Marques <65464431+mmvrstudio@users.noreply.github.com>
2022-05-18 09:04:38 -0400

    Update LoginForm.js

--- brewers/client/src/components/LoginForm.js
+++ brewers/client/src/components/LoginForm.js
@@ -3,12 +3,10 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
-
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
