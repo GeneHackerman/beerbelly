8540e395fe333bfbc9eaa0b2aaaba3a0ece97605
Mariana Marques <65464431+mmvrstudio@users.noreply.github.com>
2022-05-18 12:34:00 -0400

    Update connection.js

--- brewers/server/config/connection.js
+++ brewers/server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {  // REPLACE TH API
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
