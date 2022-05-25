1eba5a70ade66fafc4d688139425082d4d597cb5
REX <cssmith431@yahoo.com>
2022-05-25 13:20:09 -0400

    edits on auth

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,33 +1,50 @@
-const jwt = require('jsonwebtoken');
-const secret = 'mysecretsshhhhh';
-const expiration = '4h';
-module.exports = {
-  authMiddleware: function (req, res, next) {
-    let token = req.query.token || req.headers.authorization;
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+// No required changes need for GraphQL refactor 
+// Use this to decode a token and get the user's information out of it
+import decode from 'jwt-decode';
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
+// Create a new class to instantiate for a user
+class AuthService {
+  // get user data
+  getProfile() {
+    return decode(this.getToken());
+  }
 
+  // Check if user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); // handwaiving here
+  }
+
+  // Check if token is expired
+  isTokenExpired(token) {
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
     }
+  }
 
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
 
-  
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // This will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
 
-};
+export default new AuthService();
