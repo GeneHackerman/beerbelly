bff0cd646a281953dd4ea6047d0ff87044b07f84
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-05-26 18:54:08 -0400

    quick push to develop

--- brewers/client/src/index.js
+++ brewers/client/src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
